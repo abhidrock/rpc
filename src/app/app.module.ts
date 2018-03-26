18754abddea007a57d3b3645b77136eb7b1e2c42
Abhishek Kumar Singh B <abhishek.b.kumar.singh@ericsson.com>
2018-03-26 14:51:07 +0530

    create update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes, Router } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { CreateComponent } from './create/create.component';
+import { createComponent } from '@angular/compiler/src/core';
+import { UpdateComponent } from './update/update.component';
+
+const appRoutes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {
+    path: 'home', 
+    component: MainpageComponent,
+    children: [
+      //{ path: '', redirectTo: 'create', pathMatch: 'full' },
+      { path: 'create', component: CreateComponent },
+      { path: 'update', component: UpdateComponent }
+    ]
+  }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainpageComponent,
+    CreateComponent,
+    UpdateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
